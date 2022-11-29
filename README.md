b3e80b39124e7e23e80fb805a684b1f49a33dc26
三咲智子 <sxzz@sxzz.moe>
2022-11-29 14:15:10 +0800

    feat: add async & sync

--- README.md
+++ README.md
@@ -4,15 +4,21 @@
 Running "Vue JSX" suite...
 Progress: 100%
 
-  swc:
-    2 213 ops/s, ±5.39%   | slowest, 66.8% slower
+  SWC async:
+    634 ops/s, ±46.75%     | 90.54% slower
 
-  babel:
-    6 666 ops/s, ±2.42%   | fastest
+  SWC sync:
+    417 ops/s, ±29.20%     | slowest, 93.78% slower
 
-Finished 2 cases!
-  Fastest: babel
-  Slowest: swc
+  Babel sync:
+    6 629 ops/s, ±1.32%   | 1.07% slower
+
+  Babel async:
+    6 701 ops/s, ±1.87%   | fastest
+
+Finished 4 cases!
+  Fastest: Babel async
+  Slowest: SWC sync
 ```
 
 Tested on Apple M1 Max (32GB)
--- benchmark.js
+++ benchmark.js
@@ -1,13 +1,16 @@
 const code = `const Comp = () => <h1>Hello World</h1>`
 
 import b from 'benny'
-import { transform as swcTransform } from '@swc/core'
+import {
+  transform as swcTransform,
+  transformSync as swcTransformSync,
+} from '@swc/core'
 import { transform as babelTransform } from '@babel/core'
 
 b.suite(
   'Vue JSX',
 
-  b.add('swc', async () => {
+  b.add('SWC async', async () => {
     await swcTransform(code, {
       jsc: {
         parser: {
@@ -21,7 +24,28 @@ b.suite(
     })
   }),
 
-  b.add('babel', async () => {
+  b.add('SWC sync', () => {
+    swcTransformSync(code, {
+      jsc: {
+        parser: {
+          syntax: 'ecmascript',
+          jsx: true,
+        },
+        experimental: {
+          plugins: [['swc-plugin-vue-jsx', {}]],
+        },
+      },
+    })
+  }),
+
+  b.add('Babel sync', () => {
+    babelTransform(code, {
+      plugins: ['@vue/babel-plugin-jsx'],
+    })
+  }),
+
+  b.add('Babel async', async () => {
+    // https://github.com/sxzz/swc-benchmark/pull/1
     await babelTransform(code, {
       plugins: ['@vue/babel-plugin-jsx'],
     })
