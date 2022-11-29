e4a9100da70250f418dd532a3474af717247f693
三咲智子 <sxzz@sxzz.moe>
2022-11-29 10:18:20 +0800

    feat: vue jsx

---
+++ README.md
@@ -0,0 +1,18 @@
+# benchmark
+
+```
+Running "Vue JSX" suite...
+Progress: 100%
+
+  swc:
+    2 213 ops/s, ±5.39%   | slowest, 66.8% slower
+
+  babel:
+    6 666 ops/s, ±2.42%   | fastest
+
+Finished 2 cases!
+  Fastest: babel
+  Slowest: swc
+```
+
+Tested on Apple M1 Max (32GB)
---
+++ benchmark.js
@@ -0,0 +1,35 @@
+const code = `const Comp = () => <h1>Hello World</h1>`
+
+import b from 'benny'
+import { transform as swcTransform } from '@swc/core'
+import { transform as babelTransform } from '@babel/core'
+
+b.suite(
+  'Vue JSX',
+
+  b.add('swc', async () => {
+    await swcTransform(code, {
+      jsc: {
+        parser: {
+          syntax: 'ecmascript',
+          jsx: true,
+        },
+        experimental: {
+          plugins: [['swc-plugin-vue-jsx', {}]],
+        },
+      },
+    })
+  }),
+
+  b.add('babel', () => {
+    babelTransform(code, {
+      plugins: ['@vue/babel-plugin-jsx'],
+    })
+  }),
+
+  b.cycle(),
+  b.complete(),
+  b.save({
+    format: 'chart.html',
+  })
+)
